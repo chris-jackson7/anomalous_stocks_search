1398d12bee69b90d9b94319cf00fc487465136ea
Chris Jackson <86484549+chris-jackson7@users.noreply.github.com>
2024-03-10 10:04:45 -0600

    Update market_source.py

--- scripts/market_source/market_source.py
+++ scripts/market_source/market_source.py
@@ -216,7 +216,7 @@ model.fit(X_train, y_train, eval_set=[(X_test, y_test)]) # , early_stopping_roun
 # Extract Evaluation
 rmse = min(model.evals_result()['validation_0']['rmse'])
 with open('assets/model_rmse.txt', 'w') as file:
-   file.write(rmse)
+   file.write(str(rmse))
 
 # Make Predictions
 y_pred = model.predict(X_test)
